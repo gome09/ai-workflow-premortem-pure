2d66d76792872ec1bf6f5d8956c5a63f6ce25300
gome09 <3567039961@qq.com>
2026-09-01 10:21:42 +0800

    chore: 删除未接线的死代码 panels 与 api_client/state 模块

--- frontend/components/__init__.py
+++ frontend/components/__init__.py
@@ -1,7 +1,2 @@
 # frontend/components/__init__.py
 from __future__ import annotations
-
-from components.gate_panel import render_gate_panel
-from components.trace_panel import render_trace_panel
-
-__all__ = ["render_gate_panel", "render_trace_panel"]
